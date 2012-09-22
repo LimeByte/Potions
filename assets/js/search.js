ce0855cb8346a4727cfcf2caa1de7b4aff1d709c
LimeByte <limebyte@twf.co.nz>
2012-09-22 22:40:14 +1200

    Fixed misplaced variable.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,10 +1,10 @@
 $("#search-bar").on("keyup", function() {
 	var term = $(this).val().toLowerCase();
-	var value = "";
 	
 	$("#potion-table tr").each(function(index) {
 		if (index !== 0) {
 			$row = $(this);
+			var value = "";
 			
 			$("#potion-table tr td").each(function(index) {
 				value += $(this).text();
