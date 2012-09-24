77a7493ea228ca210d4fc22faa86a7ec627ae1ad
LimeByte <limebyte@twf.co.nz>
2012-09-24 16:56:31 +1200

    Finished moving JavaScript.

--- assets/js/potions.js
+++ assets/js/potions.js
@@ -1,10 +1,32 @@
+function addPotions(callback) {
+	logInfo("Adding potions...");
+	$.ajax({
+		type: "GET",
+		url: "assets/xml/potions.xml", 
+		dataType: "xml",
+		success: function(xml) {	
+			$(xml).find('potion').each(function() {	
+				addPotion(getValue(this, 'name'),
+						  getValue(this, 'icon'),
+						  getValue(this, 'id'),
+						  getValue(this, 'duration'));
+			});
+			$("#potion-table").fadeIn("slow");
+			logInfo("Done!");
+			
+			callback();
+		}
+	});
+}
+
 function addPotion(name, icon, id, duration) {
 	$('<tr></tr>').html(
-	    '<td><img src="assets/img/potions/'+icon+'.png" /></td>'
-	  + '<td>' + name + '</td>'
-	  + '<td>' + duration + '</td>'
-	  + '<td>' + id + '</td>')
-	  .appendTo('#potion-table');
+		'<td><img src="assets/img/potions/'+icon+'.png" /></td>' +
+		'<td>' + id + '</td>' +
+		'<td>' + name + '</td>' +
+		'<td>' + duration + '</td>'
+	)
+	.appendTo('#potion-table');
 	
 	logDebug('Added ' + name + ' (' + duration + ').');
 }
